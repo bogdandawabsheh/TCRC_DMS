e4121adc4a0a49239b91ad9624bdb0522956c64e
nikhilpai007 <nikhilpaiganesh@trentu.ca>
2019-11-29 14:20:57 -0500

    Export changes

--- export.php
+++ export.php
@@ -2,14 +2,14 @@
 
 if (isset($_POST["export"]))
 {
-    $connect = mysqli_connect("localhost", "root", "", "test");
+    $connect = mysqli_connect("localhost", "apollosoftware", "cois4000Y", "apollosoftware");
     header('Content-Type: text/csv; charset=utf-8');
     header('Content-Disposition: attachment; filename=data.csv');
     $output = fopen("php://output", "w");
-    fputcsv($output, array('id', 'Organization Name', 'Contact'. 'Address', 'Phone', 'Email', 'Website', 'Logo Consent', 'Organization Purpose', 'Organization Year', 'Organization Employee', 'Approved', 'Theme', 'Project Scale', 'Project Title', 'Project Description', 'Project Task', 'Project Start Date', 'Project End Date', 'Research Ethics 1', 'Research Ethics 2', 'Research Ethics 3', 'Project Implementation', 'Screening Requirements 1', 'Screening Requirements 2', 'Additional Skills', 'Resources Needed', 'Funding Needed', 'Additional Notes', 'Photo Link'));
+    fputcsv($output, array('id', 'Organization Name', 'Contact', 'Address', 'Phone', 'Email', 'Website', 'Logo Consent', 'Organization Purpose', 'Organization Year', 'Organization Employee', 'Approved', 'Theme', 'Project Scale', 'Project Title', 'Project Description', 'Project Task', 'Project Start Date', 'Project End Date', 'Research Ethics 1', 'Research Ethics 2', 'Research Ethics 3', 'Project Implementation', 'Screening Requirements 1', 'Screening Requirements 2', 'Additional Skills', 'Resources Needed', 'Funding Needed', 'Additional Notes', 'Photo Link'));
     $query = "SELECT * FROM projectFrom";
     $result = mysqli_query($connect, $query);
-    while($row = mysqli_fetch_assoc($result)
+    while($row = mysqli_fetch_assoc($result))
     {
         fputcsv($output, $row);
     }
